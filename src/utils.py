c320f209bee3ade79ced5ca42203333b40b47e35
Iyanuoluwa <iy@gidigidiolu.com>
2019-10-23 00:00:16 +0100

    include db method

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,8 @@
+import logging
+
+from pymongo.errors import DuplicateKeyError
+
+
 def format_query(s):
     pass
 
@@ -7,3 +12,13 @@ def read_keywords_from_file(filename=None):
     with open(filename) as f:
         queries = f.readlines()
     return queries
+
+
+def save_tweet_to_db(tweet=None, collection=None):
+    id_ = {"_id": tweet['id_str']}
+    new_document = {**id_, **tweet}
+    try:
+        collection.insert_one(new_document)
+    except DuplicateKeyError:
+        logging.info(f"found duplicate key: {tweet['id_str']}")
+        raise
