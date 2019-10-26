c7c59218e3edb4ae3c610b30fc9be852d8be1e0f
Iyanuoluwa <iy@gidigidiolu.com>
2019-10-26 21:41:45 +0100

    improve error handling during scraping and retweet fetching

--- src/data/download_dataset.py
+++ src/data/download_dataset.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import logging
 import os
+from collections import deque
 
 import click
 import progressbar
@@ -8,53 +9,168 @@ import pymongo
 import requests
 from dotenv import find_dotenv, load_dotenv
 from pymongo.errors import DuplicateKeyError
+from twitterscraper.tweet import Tweet
+from tweepy.error import TweepError
 
-from src.twitter import auth, get_retweets, get_tweet, scrap
+from src.twitter import auth, get_retweets, scrap
 from src.utils import save_tweet_to_db
 
 
-def process_retweets(api=None, tweets=[], collection=None):
-    bar = progressbar.ProgressBar(max_len=len(tweets))
-    for tweet in bar(tweets):
+def fetch_retweets(api=None, tweet_id=None, retweet_collection=None):
+    try:
+        retweets = get_retweets(api=api, tweet_id=tweet_id)
+        if retweets is not None:
+            for retweet in retweets:
+                try:
+                    save_tweet_to_db(tweet=retweet._json,
+                                     collection=retweet_collection)
+                except DuplicateKeyError:
+                    continue
+    except TweepError as e:
+        logging.info(e)
+        raise
+
+
+def fresh_scrap(query=None, tweets=None, limit=None):
+    if tweets is None or not isinstance(tweets, deque):
+        raise TypeError('Expected Type "collections.deque", '
+                        f'got {type(tweets)}.')
+
+    tweets_ = scrap(query=query, limit=limit)
+    tweets.extend(tweets_)
+
+
+def resume_scrap(query=None, tweets=None, tweet_collection=None):
+    if tweets is None or not isinstance(tweets, deque):
+        raise TypeError('Expected Type "collections.deque", '
+                        f'got {type(tweets)}.')
+
+    resume_query = {'is_processed': False}
+    tweets_ = tweet_collection.find(resume_query, {'_id': 0})
+    tweets_ = [Tweet(username=tweet['username'],
+                     fullname=tweet['fullname'],
+                     user_id=tweet['user_id'],
+                     tweet_id=tweet['tweet_id'],
+                     tweet_url=tweet['tweet_url'],
+                     timestamp=tweet['timestamp'],
+                     timestamp_epochs=tweet['timestamp_epochs'],
+                     replies=tweet['replies'],
+                     retweets=tweet['retweets'],
+                     likes=tweet['likes'],
+                     is_retweet=tweet['is_retweet'],
+                     retweeter_username=tweet['retweeter_username'],
+                     retweeter_userid=tweet['retweeter_userid'],
+                     retweet_id=tweet['retweet_id'],
+                     text=tweet['text'],
+                     html=tweet['html']) for tweet in tweets_]
+
+    logging.info(f'{len(tweets_)} unprocessed tweets have been queued for '
+                 'processing.')
+    tweets.extend(tweets_)
+
+
+def to_fetch_retweets(tweet_id, tweet_collection):
+    id_query = {'_id': tweet_id}
+    result = tweet_collection.find_one(id_query)
+    return not result or not result['is_processed']
+
+
+def process_retweets(api=None, tweets=None, tweet_collection=None,
+                     retweet_collection=None):
+    bar = progressbar.ProgressBar(max_value=len(tweets))
+    bar.start()
+    while tweets:
+        tweet = tweets[-1]
         if not tweet.is_retweet:
-            retweets = get_retweets(api=api,
-                                    tweet_id=tweet.tweet_id)
-            if retweets:
-                for retweet in retweets:
-                    try:
-                        save_tweet_to_db(tweet=retweet._json,
-                                         collection=collection)
-                    except DuplicateKeyError:
-                        continue
-        else:
-            tweet_ = get_tweet(api=api, tweet_id=tweet.tweet_id)
-            try:
-                save_tweet_to_db(tweet=tweet_._json, collection=collection)
-            except DuplicateKeyError:
-                continue
+            if to_fetch_retweets(tweet_id=tweet.tweet_id,
+                                 tweet_collection=tweet_collection):
+                try:
+                    fetch_retweets(api=api, tweet_id=tweet.tweet_id,
+                                   retweet_collection=retweet_collection)
+                    new_document = {'_id': tweet.tweet_id,
+                                    'is_processed': True,
+                                    'username': tweet.username,
+                                    'fullname': tweet.fullname,
+                                    'user_id': tweet.user_id,
+                                    'tweet_id': tweet.tweet_id,
+                                    'tweet_url': tweet.tweet_url,
+                                    'timestamp': tweet.timestamp,
+                                    'timestamp_epochs': tweet.timestamp_epochs,
+                                    'replies': tweet.replies,
+                                    'retweets': tweet.retweets,
+                                    'likes': tweet.likes,
+                                    'is_retweet': tweet.is_retweet,
+                                    'retweeter_username': tweet.retweeter_username,
+                                    'retweeter_userid': tweet.retweeter_userid,
+                                    'retweet_id': tweet.retweet_id,
+                                    'text': tweet.text,
+                                    'html': tweet.html,
+                                    }
+                    tweet_collection.insert_one(new_document)
+                except DuplicateKeyError:
+                    tweet_query = {'_id': tweet.tweet_id}
+                    new_values = {"$set": {"is_processed": True}}
+                    tweet_collection.update_one(tweet_query, new_values)
+                except TweepError:
+                    _ = tweets.pop()
+                    bar += 1
+                    continue
+        _ = tweets.pop()
+        bar += 1
+    bar.finish()
+
+
+def process_left_over_tweets(tweets=None, tweet_collection=None):
+    bar = progressbar.ProgressBar(max_value=len(tweets))
+    bar.start()
+    while tweets:
+        tweet = tweets.pop()
+        try:
+            new_document = {'_id': tweet.tweet_id,
+                            'is_processed': False,
+                            'username': tweet.username,
+                            'fullname': tweet.fullname,
+                            'user_id': tweet.user_id,
+                            'tweet_id': tweet.tweet_id,
+                            'tweet_url': tweet.tweet_url,
+                            'timestamp': tweet.timestamp,
+                            'timestamp_epochs': tweet.timestamp_epochs,
+                            'replies': tweet.replies,
+                            'retweets': tweet.retweets,
+                            'likes': tweet.likes,
+                            'is_retweet': tweet.is_retweet,
+                            'retweeter_username': tweet.retweeter_username,
+                            'retweeter_userid': tweet.retweeter_userid,
+                            'retweet_id': tweet.retweet_id,
+                            'text': tweet.text,
+                            'html': tweet.html,
+                            }
+            tweet_collection.insert_one(new_document)
+        except DuplicateKeyError:
+            bar += 1
+            continue
+        bar += 1
+    bar.finish()
 
 
 @click.command()
 @click.argument('topic')
 @click.argument('query', nargs=-1)
-@click.option('--limit', type=click.INT, default=-1)
-def main(topic, query, limit):
-    """ Downloads Users' Tweets
+@click.option('--limit', type=click.INT, default=5)
+@click.option('--resume', is_flag=True)
+def main(topic, query, limit, resume):
+    """ Downloads Users' Tweetsd
     """
     logger = logging.getLogger(__name__)
-
-    url = "http://example.com/"
-    timeout = 5
 
     client = None
     db = None
 
-    tweets = []
-    try:
-        # test internet conncetivity is active
-        req = requests.get(url, timeout=timeout)
-        req.raise_for_status()
+    # ensure topic is in lowercase
+    topic = topic.lower()
 
+    tweets = deque()
+    try:
         # prepare credentials for accessing twitter API
         consumer_key = os.environ.get('CONSUMER_KEY')
         consumer_secret = os.environ.get('CONSUMER_SECRET')
@@ -71,21 +187,48 @@ def main(topic, query, limit):
                                      appname=__file__)
 
         db = client[db_name]
-        topic_collection = db[topic]
+        tweet_collection_name = topic + '-tweets'
+        tweet_collection = db[tweet_collection_name]
+        retweet_collection = db[topic + '-retweets']
+
         query = ' '.join(query)
 
-        logger.info(f'scrapping tweets for "{query}"')
-        tweets = scrap(query=query, limit=limit)
+        collections = db.list_collection_names()
+        if tweet_collection_name not in collections:
+            logger.info(f'scrapping tweets for "{query}"')
+            fresh_scrap(query=query, tweets=tweets, limit=limit)
+
+        if tweet_collection_name in collections and not resume:
+            logger.info(f'scrapping tweets for "{query}"')
+            fresh_scrap(query=query, tweets=tweets, limit=limit)
+
+            logger.info(f'fetching previously unprocessed tweets for {query}')
+            resume_scrap(query=query, tweets=tweets,
+                         tweet_collection=tweet_collection)
+
+        if tweet_collection_name in collections and resume:
+            logger.info(f'resuming scrapping for {query}')
+            resume(query=query, tweets=tweets,
+                   tweet_collection=tweet_collection)
+
+        logger.info(f'processing retweets')
+        process_retweets(api=api, tweets=tweets,
+                         tweet_collection=tweet_collection,
+                         retweet_collection=retweet_collection)
+
     except requests.exceptions.HTTPError as e:
         logger.error("Checking internet connection failed, "
                      f"status code {e.response.status_code}")
     except requests.exceptions.ConnectionError:
-        logger.error("No internet connection available.")
+        logger.error("Could not establish a connection.")
+    except (ValueError, TypeError) as e:
+        logger.error(e)
+    except TweepError as e:
+        logger.error(e)
     finally:
-        if tweets:
-            logger.info(f'processing retweets')
-            process_retweets(api=api, tweets=tweets,
-                             collection=topic_collection)
+        logger.info('saving unprocessed tweets')
+        process_left_over_tweets(tweets=tweets,
+                                 tweet_collection=tweet_collection)
 
         if client is not None:
             logger.info('ending all server sessions')
