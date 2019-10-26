6e9b1d190adc39d70cf3a974b2adf8399fcce0aa
Iyanuoluwa <iy@gidigidiolu.com>
2019-10-26 21:39:57 +0100

    handle tweep errors

--- src/twitter.py
+++ src/twitter.py
@@ -1,8 +1,4 @@
-import logging
-
-import progressbar
 import tweepy
-from pymongo.errors import DuplicateKeyError
 from twitterscraper import query_tweets
 
 
@@ -27,45 +23,26 @@ def scrap(query=None, limit=-1):
 
 
 def get_retweets(api=None, tweet_id=None, count=None):
-    if count is None:
-        retweets = api.retweets(tweet_id)
-    else:
-        retweets = api.retweets(tweet_id, count)
+    retweets = None
+
+    try:
+        if count is None:
+            retweets = api.retweets(tweet_id)
+        else:
+            retweets = api.retweets(tweet_id, count)
+    except tweepy.error.TweepError:
+        raise
 
     return retweets
 
 
 def get_tweet(api=None, tweet_id=None):
-    status = api.get_status(tweet_id)
-    return status._json
+    status = None
 
+    try:
+        status = api.get_status(tweet_id)
+        status = status._json
+    except tweepy.error.TweepError:
+        raise
 
-def search(api=None, query=None, db=None, n_tweets=1000):
-    """Returns a collection of relevant Tweets matching a specified query.
-
-    Keyword Arguments:
-        api {[type]} -- [description] (default: {None})
-        query {[type]} -- [description] (default: {None})
-        collection {[type]} -- [description] (default: {None})
-
-    Raises:
-        tweepy.TweepError: [description]
-    """
-    logging.info(f"Fetching Tweet Result for {query}.")
-
-    topic_collection = query.split()[0]
-    collection = db[topic_collection]
-
-    bar = progressbar.ProgressBar()
-    cursor = tweepy.Cursor(api.search, q=query, count=100, lang='en',
-                           tweet_mode='extended').items(n_tweets)
-    for status in bar(cursor):
-        tweet = status._json
-
-        id_ = {"_id": tweet['id_str']}
-        new_document = {**id_, **tweet}
-        try:
-            collection.insert_one(new_document)
-        except DuplicateKeyError:
-            logging.info(f"found duplicate key: {tweet['id_str']}")
-            continue
+    return status
